a69f9ae0e8e8013925ef06870fb000c1873dfba7
moraiskaua <moraiskaua03@gmail.com>
2023-11-10 17:45:53 -0300

    🧪 UserEntity integration name field tests

---
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -0,0 +1,27 @@
+import { EntityValidationError } from '@/shared/domain/errors/validation-error';
+import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
+describe('UserEntity integration tests', () => {
+  describe('Constructor method', () => {
+    it('Should throw an error if the name is invalid', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        name: null,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: '',
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 'a'.repeat(256),
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
+  });
+});
