4cb82fd7c3471016b0c9908a4c865a1157950c69
moraiskaua <moraiskaua03@gmail.com>
2023-11-10 19:02:04 -0300

    👨‍💻 InMemoryRepository

---
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+import { Entity } from '../entities/entity';
+import { NotFoundError } from '../errors/not-found-error';
+import { RepositoryInterface } from './repository-contracts';
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const index = this.items.findIndex(item => item.id === entity.id);
+    this.items[index] = entity;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this._get(id);
+    this.items = this.items.filter(item => item.id !== id);
+  }
+
+  async findById(id: string): Promise<E> {
+    return this._get(id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const _id = `${id}`;
+    const entity = this.items.find(item => item.id === _id);
+
+    if (!entity) {
+      throw new NotFoundError('Entity not found');
+    }
+
+    return entity;
+  }
+}
