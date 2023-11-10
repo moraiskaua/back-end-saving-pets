916f07ec8fe92e962ee8f7abf741483e4bdcb18d
moraiskaua <moraiskaua03@gmail.com>
2023-11-10 17:28:48 -0300

    🧪 UserEntity constructor unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -6,46 +6,54 @@ describe('UserEntity unit tests', () => {
   let sut: UserEntity;
 
   beforeEach(() => {
+    UserEntity.validate = jest.fn();
     props = UserDataBuilder({});
     sut = new UserEntity(props);
   });
 
-  it('should create a new user entity', () => {
+  it('Constructor should call validate method', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
+    expect(sut.props).toEqual(props);
+  });
+
+  it('Should create a new user entity', () => {
     expect(sut.props).toEqual(props);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 
-  it('should return the user name', () => {
+  it('Should return the user name', () => {
     expect(sut.props.name).toBeDefined();
     expect(sut.props.name).toEqual(props.name);
     expect(typeof sut.props.name).toBe('string');
   });
 
-  it('should update the user name', () => {
+  it('Should update the user name', () => {
     const newName = 'new name';
     sut.update(newName);
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual(newName);
   });
 
-  it('should return the user email', () => {
+  it('Should return the user email', () => {
     expect(sut.props.email).toBeDefined();
     expect(sut.props.email).toEqual(props.email);
     expect(typeof sut.props.email).toBe('string');
   });
 
-  it('should return the user password', () => {
+  it('Should return the user password', () => {
     expect(sut.props.password).toBeDefined();
     expect(sut.props.password).toEqual(props.password);
     expect(typeof sut.props.password).toBe('string');
   });
 
-  it('should update the user password', () => {
+  it('Should update the user password', () => {
     const newPassword = 'new password';
     sut.updatePassword(newPassword);
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.password).toEqual(newPassword);
   });
 
-  it('should return the user created_at', () => {
+  it('Should return the user created_at', () => {
     expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
