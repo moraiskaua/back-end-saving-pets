d8259213cd22a4c093f76b7e0e878c7e8a093211
moraiskaua <moraiskaua03@gmail.com>
2023-11-10 17:50:36 -0300

    🧪 UserEntity integration email field tests

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -22,6 +22,38 @@ describe('UserEntity integration tests', () => {
         name: 'a'.repeat(256),
       };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 10 as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
+
+    it('Should throw an error if the email is invalid', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        email: null,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: '',
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256),
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 10 as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
     });
   });
 });
