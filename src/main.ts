e3b9e4231fdaae629b9e21eedb2c0292a0310897
moraiskaua <moraiskaua03@gmail.com>
2023-11-23 00:09:24 -0300

    🚀 Trying deploy 3

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,22 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
+import fastifyCors from 'fastify-cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-    { cors: true },
   );
-  app.enableCors({
+
+  app.register(fastifyCors, {
     origin: 'https://front-end-saving-pets.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   applyGlobalConfig(app);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
